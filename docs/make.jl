ca40ff327ca58bb9f4f0501d9d83606bc7aefe25
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-06-26 21:28:54 -0400

    pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,13 @@
 using Documenter, Margins
 
-makedocs(sitename = "Margins.jl")
+makedocs(
+  sitename = "Margins.jl",
+  modules  = [Margins],
+  formats  = Documenter.HTML(),
+)
 
 deploydocs(
-    repo = "github.com/emfeltham/Margins.jl.git",
-    target = "build",
+  repo   = "https://github.com/emfeltham/Margins.jl.git",
+  branch = "main",    # default branch
+  folder = "docs",    # the target folder on main
 )
