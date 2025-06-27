8bf6137358405c7fa187d6b10ab050e961e0569c
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2025-06-27 08:56:40 -0400

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,22 @@
 using Documenter, Margins
 
-makedocs(; modules=[Margins],
-         authors="Eric Feltham",
-         repo="https://github.com/emfeltham/Margins.jl/blob/{commit}{path}#{line}",
-         sitename="Margins.jl",
-         format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
-                                repolink="https://github.com/emfeltham/Margins.jl",
-                                # canonical="https://beacon-biosignals.github.io/Effects.jl/stable",
-                                assets=String[]),
-         pages=["Home" => "index.md"])
+makedocs(;
+  modules = [Margins],
+  authors = "Eric Feltham",
+  repo = "https://github.com/emfeltham/Margins.jl/blob/{commit}{path}#{line}",
+  sitename = "Margins.jl",
+  format = Documenter.HTML(
+    ; prettyurls = get(ENV, "CI", "false") == "true",
+    repolink = "https://github.com/emfeltham/Margins.jl",
+    # canonical="https://beacon-biosignals.github.io/Effects.jl/stable",
+    assets = String[]),
+  pages = ["Home" => "index.md"],
+  doctest    = false, # do not test docs
+  checkdocs  = :none # ignore missing docstrings
+)
 
-deploydocs(; repo="https://github.com/emfeltham/Margins.jl",
-           devbranch="main",
-           push_preview=true)
+deploydocs(;
+  repo = "https://github.com/emfeltham/Margins.jl",
+  devbranch = "main",
+  push_preview = true
+)
