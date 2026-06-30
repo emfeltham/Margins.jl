3b4f9b7a115e20bee728baf248d186ae67f7bea5
Eric Martin Feltham <eric.feltham@aya.yale.edu>
2026-06-30 13:07:17 +0000

    build based on f59d36f

--- versions.js
+++ versions.js
@@ -1,6 +1,9 @@
 var DOC_VERSIONS = [
   "stable",
-  "v2.0.0",
+  "v2.1.4",
+  "v2.1.3",
+  "v2.1.2",
+  "v2.0.3",
 ];
-var DOCUMENTER_NEWEST = "v2.0";
+var DOCUMENTER_NEWEST = "v2.1.4";
 var DOCUMENTER_STABLE = "stable";
